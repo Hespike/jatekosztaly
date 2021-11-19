cb9e6b4c559fa88033665d572d8b3b4ef32d7732
Sárdi András <sardi.andras@gmail.com>
2021-11-19 11:47:48 +0100

    Create jatekosztaly.cpp

---
+++ jatekosztaly.cpp
@@ -0,0 +1,31 @@
+#include <string>
+#include <iostream>
+
+using namespace std;
+
+class Jatek {
+   private:
+       string forraskod;
+   public:
+       string nev;
+       float ar;
+   protected:
+       unsigned long megjelenesi_datum;
+};
+
+/*
+#include <string>
+
+using namespace std;
+
+class Jatek {
+public:
+   string nev;
+   float ar;
+protected:
+   unsigned long megjelenesi_datum;
+private:
+   string forraskod;
+};
+*/
+
